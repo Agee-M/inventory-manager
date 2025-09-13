7c7756efaa0169f260e2229c42111c0cda9794bd
ama3248 <ama3248@wgu.com>
2025-09-12 19:19:25 -0700

    Update BootStrapData
    - Added 2 parts to inhouse
    - Added 3 parts to outsource
    - Added 5 products
    - Slight changes to about.html

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 > - Prompt, file name, line number, and change.
 ### C
 
+> ### TODO:
+> * Ask about extra requirement here
 > ### Prompt:
 > > Customize the HTML user interface for your customer’s application. The user interface should include the shop name, the product names, and the names of the parts.
 > ### File Names:
@@ -16,25 +18,34 @@
 > Line 19
 > * Replaced "Shop" with shop name
 > 
-> Added part and product names utilizing website functionality
+> Added part and product names utilizing current website functionality
 ### D
 > ### TODO:
+> * Ask if you need to include the lines for about.html and AboutScreenController
 > ### Prompt
 > > Add an “About” page to the application to describe your chosen customer’s company to web viewers and include navigation to and from the “About” page and the main screen.
 > ### File Names
 >   mainscreen.html, about.html, AboutScreenController
 > ### Changes
-> * Created about page with about.html and AboutScreenController
-> * Added <a> to mainscreeen and about to link to each other 
+> Controllers directory
+> * Created AboutScreenController
+> 
+> Templates directory
+> * Created about.html with link to mainscreen
+> 
+> Line 20 (mainscreen.html)
+> * Added link to about.html 
 ### E
 > ### TODO:
-> *
+> * Ask if I need to include setID for outsource
 > ### Prompt
->
+> > Add a sample inventory appropriate for your chosen store to the application. You should have five parts and five products in your sample inventory and should not overwrite existing data in the database.
 > ### File Names
->
+> BootStrapData
 > ### Changes
->
+> Lines 66 - 121
+> * Added 5 parts and 5 products in
+> * Made it so that it doesn't duplicate when running multiple times
 ### F
 > > ### TODO:
 > *
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,10 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -29,49 +31,123 @@ public class BootStrapData implements CommandLineRunner {
     private final ProductRepository productRepository;
 
     private final OutsourcedPartRepository outsourcedPartRepository;
+    private final InhousePartRepository inhousePartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository, InhousePartRepository inhousePartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository = inhousePartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        // Add default objects to Parts
+        if (partRepository.count() == 0) {
+
+            /*
+            OutsourcedPart o= new OutsourcedPart();
+            o.setCompanyName("Western Governors University");
+            o.setName("out test");
+            o.setInv(5);
+            o.setPrice(20.0);
+            o.setId(100L);
+            outsourcedPartRepository.save(o);
+            OutsourcedPart thePart=null;
+            List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                if(part.getName().equals("out test"))thePart=part;
+            }
+
+            System.out.println(thePart.getCompanyName());
+            */
+
+            // Part 1 - Inhouse
+            InhousePart deck = new InhousePart();
+            deck.setName("Deck");
+            deck.setInv(5);
+            deck.setPrice(50.0);
+            deck.setId(1);
+            inhousePartRepository.save(deck);
+            InhousePart thePart = null;
+
+
+            // Part 2 - Inhouse
+            InhousePart wheels = new InhousePart();
+            wheels.setName("wheels");
+            wheels.setInv(12);
+            wheels.setPrice(25.0);
+            wheels.setId(2);
+            inhousePartRepository.save(wheels);
+
+
+            // Part 3 - Outsourced
+            OutsourcedPart grip = new OutsourcedPart();
+            grip.setCompanyName("Hold On");
+            grip.setName("Grip Tape");
+            grip.setInv(3);
+            grip.setPrice(5.0);
+            grip.setId(3);
+            outsourcedPartRepository.save(grip);
+
+
+            // Part 4 - Outsourced
+            OutsourcedPart trucks = new OutsourcedPart();
+            trucks.setCompanyName("Thunder Trucks");
+            trucks.setName("Trucks");
+            trucks.setInv(4);
+            trucks.setPrice(10.0);
+            trucks.setId(4);
+            outsourcedPartRepository.save(trucks);
+
+            // Part 5 - Outsourced
+            OutsourcedPart screws = new OutsourcedPart();
+            screws.setCompanyName("JR Fasteners");
+            screws.setName("Screws");
+            screws.setInv(5);
+            screws.setPrice(1.0);
+            screws.setId(30);
+            outsourcedPartRepository.save(screws);
+
+            // Print out parts to console
+            List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
+            for(OutsourcedPart part:outsourcedParts){
+                System.out.println(part.getName()+" "+part.getCompanyName());
+            }
+            List<InhousePart> inhouseParts=(List<InhousePart>) inhousePartRepository.findAll();
+            for(InhousePart part:inhouseParts){
+                System.out.println(part.getName()+" "+"Deck It Out");
+            }
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
+        // Add default objects to Products
+        if (productRepository.count() == 0) {
+
+            /*
+            Product bicycle= new Product("bicycle",100.0,15);
+            Product unicycle= new Product("unicycle",100.0,15);
+            productRepository.save(bicycle);
+            productRepository.save(unicycle);
+            */
+
+            Product personal = new Product("Personal Skateboard", 300.0, 5);
+            Product t1 = new Product("Tier 1 Skateboard", 250.0, 10);
+            Product t2 = new Product("Tier 2 Skateboard", 200.0, 8);
+            Product t3 = new Product("Tier 3 Skateboard", 150.0, 16);
+            Product t4 = new Product("Tier 4 Skateboard", 100.0, 25);
+            productRepository.save(personal);
+            productRepository.save(t1);
+            productRepository.save(t2);
+            productRepository.save(t3);
+            productRepository.save(t4);
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
-
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
-        System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
-        System.out.println(partRepository.findAll());
+            System.out.println("Started in Bootstrap");
+            System.out.println("Number of Products"+productRepository.count());
+            System.out.println(productRepository.findAll());
+            System.out.println("Number of Parts"+partRepository.count());
+            System.out.println(partRepository.findAll());
 
     }
 }
